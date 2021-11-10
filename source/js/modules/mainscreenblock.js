1e7b8f0532cfea5054692f7c69ab1d3528323af1
mariata06 <slinkywizard@mail.ru>
2021-11-10 22:56:14 +0300

    Анимация синего экрана

--- source/js/modules/mainscreenblock.js
+++ source/js/modules/mainscreenblock.js
@@ -1,9 +1,18 @@
 const blueBlock = document.querySelector('.main-screen__block');
 
 const animateBlock = function () {
+  let flag = true;
   window.addEventListener('click', function (evt) {
     evt.preventDefault();
-    blueBlock.classList.toggle('main-screen__block--animate');
+    if (flag) {
+      blueBlock.classList.add('main-screen__block--animate');
+      blueBlock.classList.remove('main-screen__block--animateback');
+      flag = !flag;
+    } else {
+      blueBlock.classList.add('main-screen__block--animateback');
+      blueBlock.classList.remove('main-screen__block--animate');
+      flag = !flag;
+    }
   });
 };
 
