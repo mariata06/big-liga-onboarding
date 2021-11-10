14f0f2465a9382a0874ea044c68a01f475560ec1
mariata06 <slinkywizard@mail.ru>
2021-11-10 20:21:42 +0300

    Поправила mainscreenblock.js и css

--- source/js/modules/mainscreenblock.js
+++ source/js/modules/mainscreenblock.js
@@ -3,7 +3,7 @@ const blueBlock = document.querySelector('.main-screen__block');
 const animateBlock = function () {
   window.addEventListener('click', function (evt) {
     evt.preventDefault();
-    blueBlock.classList.add('main-screen__block--animate');
+    blueBlock.classList.toggle('main-screen__block--animate');
   });
 };
 
